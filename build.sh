29bc10b9321a2712f7fc8e0cf6c6dad291eb44c3
Massimo Candela <massimo@us.ntt.net>
2026-05-07 18:28:22 +0200

    fixed release process

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ npm ci --silent
 
 npm run compile
 
-./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-win-x64 --output bin/prefixlen-finder-win-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-win-x64 --output bin/prefixlen-finder-win-x64
 
-./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-linux-x64 --output bin/prefixlen-finder-linux-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-linux-x64 --output bin/prefixlen-finder-linux-x64
 
-./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-macos-x64 --output bin/prefixlen-finder-macos-x64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-macos-x64 --output bin/prefixlen-finder-macos-x64
 
-./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-macos-arm64 --output bin/prefixlen-finder-macos-arm64 --loglevel=error
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-macos-arm64 --output bin/prefixlen-finder-macos-arm64
 
 echo "--> Prefixlen finder compiled in bin/"
 
