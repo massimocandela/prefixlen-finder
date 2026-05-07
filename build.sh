38490a6a31ec2caee752641c30e010fcffee2fc1
Massimo Candela <massimo@us.ntt.net>
2026-05-07 18:21:52 +0200

    switched to new kpg package and added nvmrc

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm -rf bin
+mkdir bin
+
+rm -rf dist
+mkdir dist
+
+npm ci --silent
+
+npm run compile
+
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-win-x64 --output bin/prefixlen-finder-win-x64 --loglevel=error
+
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-linux-x64 --output bin/prefixlen-finder-linux-x64 --loglevel=error
+
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-macos-x64 --output bin/prefixlen-finder-macos-x64 --loglevel=error
+
+./node_modules/.bin/pkg ./package.json --options "no-warnings,max-old-space-size=4096" --targets node22-macos-arm64 --output bin/prefixlen-finder-macos-arm64 --loglevel=error
+
+echo "--> Prefixlen finder compiled in bin/"
+
